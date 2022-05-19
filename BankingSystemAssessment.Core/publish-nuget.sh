1f8efb897f474e6dbe5565431b390d772febaa6b
xdhm <xdhm@mjolner.dk>
2022-05-19 11:47:59 +0200

    update core package

--- BankingSystemAssessment.Core/publish-nuget.sh
+++
@@ -1,4 +0,0 @@
-dotnet nuget update source github --source https://nuget.pkg.github.com/DohaAbdelkarim/index.json --username DohaAbdelkarim --password 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 --store-password-in-clear-text
-
-dotnet build BankingSystemAssessment.Core.csproj
-dotnet nuget push --source "github" --api-key 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 bin/Debug/BankingSystemAssessment.Core.1.0.0.nupkg
